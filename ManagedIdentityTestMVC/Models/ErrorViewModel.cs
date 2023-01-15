d1b11916ca906b8c23173373910878bd5bd413b3
Nazir Ahmad <nazirahmad@microsoft.com>
2023-01-15 16:02:09 +0530

    added new mvc webapp

---
+++ ManagedIdentityTestMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManagedIdentityTestMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
