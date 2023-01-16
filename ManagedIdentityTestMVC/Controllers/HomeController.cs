2b3112772489dc78e6ec7a5e4d497c8531e2e78a
Nazir Ahmad <i.am.nazir.ahmad@gmail.com>
2023-01-16 18:59:17 +0530

    use web app system identity for token (#3)
    Co-authored-by: Nazir Ahmad <nazirahmad@microsoft.com>

--- ManagedIdentityTestMVC/Controllers/HomeController.cs
+++ ManagedIdentityTestMVC/Controllers/HomeController.cs
@@ -2,9 +2,7 @@
 using Azure.Identity;
 using ManagedIdentityTestMVC.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Cosmos;
 using System.Diagnostics;
-using static System.Net.WebRequestMethods;
 
 namespace ManagedIdentityTestMVC.Controllers
 {
@@ -24,8 +22,7 @@ namespace ManagedIdentityTestMVC.Controllers
 
         public IActionResult Privacy()
         {
-            var clientId = "d6e0b510-ae0c-4218-bb78-e5615ebe8b6e";
-            GetAccessToken(clientId);
+            GetAccessToken();
             return View();
         }
 
@@ -35,25 +32,15 @@ namespace ManagedIdentityTestMVC.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        private void GetAccessToken(string clientId)
+        private void GetAccessToken()
         {
             try
             {
-                var credOptions = new DefaultAzureCredentialOptions
-                {
-                    ManagedIdentityClientId = clientId,
-                    //TenantId = "b1914422-c6d8-4aec-b71b-3aa233f6165e"
-                };
-                var credential = new ManagedIdentityCredential(clientId);
+                var credential = new ManagedIdentityCredential();
                 var accessToken = credential.GetToken(new TokenRequestContext(new[] { "https://cosmos.documents.azure.com//.default" }));
                 // To print the token, you can convert it to string 
                 var accessTokenString = accessToken.Token.ToString();
                 ViewBag.AccessToken = accessTokenString;
-
-                //var cosmosUrl = @"https://cosmos-eus-test.documents.azure.com:443/";
-                //var options = new CosmosClientOptions() { ConnectionMode = ConnectionMode.Gateway };
-                //var client = new CosmosClient(cosmosUrl, credential, options);
-                //var res = client.CreateDatabaseIfNotExistsAsync("Office").GetAwaiter().GetResult();
             }
             catch (Exception ex)
             {
